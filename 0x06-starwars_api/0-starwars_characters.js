719971d60d3c4ff876bc2fe93bd65b51b5c47567
Mahao05 <prudencemolefe93@gmail.com>
2024-11-17 16:39:40 -0800

    made minor changes

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,11 +1,44 @@
 #!/usr/bin/node
 
-let request = “https://swapi-api.alx-tools.com/api/people/”
+const axios = require('axios');
 
-fetch(request).then((response) => {
-    return response.json();
-}).then( (data) => {
-    let p = document.getElementId(“Movie”);
-    console.log(data);
-    p.innerHTML = JSON.stringify(data.characters);
-})
+const fetchCharacters = async (movieId) => {
+    const baseUrl = 'https://swapi-api.alx-tools.com/api/films/';
+
+    try {
+        const movieResponse = await axios.get(`${baseUrl}${movieId}/`);
+        const movieData = movieResponse.data;
+
+        console.log(`Characters in '${movieData.title}':`);
+
+        for (const characterUrl of movieData.characters) {
+            try {
+                const characterResponse = await axios.get(characterUrl);
+                console.log(characterResponse.data.name);
+            } catch (error) {
+                console.error(`Error fetching character data: ${error.message}`);
+            }
+        }
+    } catch (error) {
+        console.error(`Error fetching movie data: ${error.message}`);
+    }
+};
+
+const main = () => {
+    const args = process.argv.slice(2);
+
+    if (args.length !== 1) {
+        console.log('Usage: node script.js <Movie ID>');
+        process.exit(1);
+    }
+
+    const movieId = args[0];
+    if (isNaN(movieId)) {
+        console.log('Movie ID must be a number.');
+        process.exit(1);
+    }
+
+    fetchCharacters(movieId);
+};
+
+main();
